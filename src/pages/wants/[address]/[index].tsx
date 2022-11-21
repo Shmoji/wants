5e1e0c49c8d8e37413140c452c65dfcdaa19f6c6
Shmoji <jackj_skate@hotmail.com>
2022-11-21 17:05:24 -0500

    no idea what this is but im commiting it

--- src/pages/wants/[address]/[index].tsx
+++ src/pages/wants/[address]/[index].tsx
@@ -1,4 +1,3 @@
-import { useWeb3React } from "@web3-react/core"
 import DefaultLayout from "components/layouts/DefaultLayout"
 import { PersonalWant } from "constants"
 import { GetServerSideProps } from "next"
